1066d2d8d8e50e26e68e20e6048f20207dc025d9
Novandika <noyupiirin@gmail.com>
2022-10-19 18:57:06 +0700

    bumdes v1

---
+++ keuangan.sql
@@ -0,0 +1,328 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2022 at 02:14 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.0.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `keuangan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `pass` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama`, `email`, `pass`) VALUES
+(1, 'novandika', 'novandika@gmail.com', 'admin123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catatan`
+--
+
+CREATE TABLE `catatan` (
+  `id_catatan` int(11) NOT NULL,
+  `catatan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `catatan`
+--
+
+INSERT INTO `catatan` (`id_catatan`, `catatan`) VALUES
+(1, 'Semangat Nggeh'),
+(2, 'Hari Kamis jam 8 akan ada rapat, diharapkan kepada semua karyawan agar dapat berhadir.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hutang`
+--
+
+CREATE TABLE `hutang` (
+  `id_hutang` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `tgl_hutang` date NOT NULL,
+  `alasan` text NOT NULL,
+  `penghutang` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hutang`
+--
+
+INSERT INTO `hutang` (`id_hutang`, `jumlah`, `tgl_hutang`, `alasan`, `penghutang`) VALUES
+(5, 0, '2019-10-19', '', ''),
+(6, 0, '2019-10-20', '', ''),
+(9, 0, '2019-10-23', '', ''),
+(18, 250000, '2022-10-13', 'Dukuh', 'Adi rahman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `posisi` varchar(40) NOT NULL,
+  `alamat` varchar(40) NOT NULL,
+  `umur` int(11) NOT NULL,
+  `kontak` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`id_karyawan`, `nama`, `posisi`, `alamat`, `umur`, `kontak`) VALUES
+(1, 'Beki', 'ketua', 'Kediri', 19, '0888888'),
+(2, 'belike', 'Bendahara', 'Aceh', 19, '08333333333');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemasukan`
+--
+
+CREATE TABLE `pemasukan` (
+  `id_pemasukan` int(11) NOT NULL,
+  `tgl_pemasukan` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `id_sumber` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pemasukan`
+--
+
+INSERT INTO `pemasukan` (`id_pemasukan`, `tgl_pemasukan`, `jumlah`, `id_sumber`) VALUES
+(1, '2019-10-16', 100000, 1),
+(2, '2019-10-24', 500000, 1),
+(3, '2019-10-17', 200000, 5),
+(4, '2019-10-18', 400000, 3),
+(5, '2019-10-19', 5000000, 1),
+(6, '2019-10-20', 100000, 4),
+(7, '2019-10-21', 2300000, 3),
+(8, '2019-10-22', 2000000, 2),
+(9, '2019-10-23', 1500000, 5),
+(10, '2019-10-15', 100000, 1),
+(13, '2019-10-17', 200000, 1),
+(14, '2019-10-09', 200000, 1),
+(15, '2019-10-19', 200000, 3),
+(16, '2019-10-02', 200000, 4),
+(17, '2019-10-07', 20000, 5),
+(18, '2019-10-26', 50000, 1),
+(19, '2019-10-27', 2000000, 2),
+(20, '2019-10-28', 590000, 3),
+(21, '2019-10-29', 600000, 4),
+(22, '2019-10-30', 600000, 5),
+(23, '2019-10-25', 7000000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengeluaran`
+--
+
+CREATE TABLE `pengeluaran` (
+  `id_pengeluaran` int(11) NOT NULL,
+  `tgl_pengeluaran` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `id_sumber` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengeluaran`
+--
+
+INSERT INTO `pengeluaran` (`id_pengeluaran`, `tgl_pengeluaran`, `jumlah`, `id_sumber`) VALUES
+(1, '2019-10-22', 1100000, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sumber`
+--
+
+CREATE TABLE `sumber` (
+  `id_sumber` int(11) NOT NULL,
+  `nama` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sumber`
+--
+
+INSERT INTO `sumber` (`id_sumber`, `nama`) VALUES
+(1, 'Buat Web Pemerintah'),
+(2, 'Desain Poster Lomba'),
+(3, 'Instalasi Softwre'),
+(4, 'Instalasi OS'),
+(5, 'Buat Video Animasi'),
+(6, 'Domain'),
+(7, 'Hosting'),
+(8, 'Listrik'),
+(9, 'Air'),
+(10, 'Wifi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uang`
+--
+
+CREATE TABLE `uang` (
+  `id_uang` int(11) NOT NULL,
+  `tgl_uang` date NOT NULL,
+  `id_pengeluaran` int(11) DEFAULT NULL,
+  `id_pendapatan` int(11) DEFAULT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `uang`
+--
+
+INSERT INTO `uang` (`id_uang`, `tgl_uang`, `id_pengeluaran`, `id_pendapatan`, `jumlah`) VALUES
+(1, '2019-10-23', NULL, 1, 500000),
+(2, '2019-10-24', 2, NULL, 10000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `catatan`
+--
+ALTER TABLE `catatan`
+  ADD PRIMARY KEY (`id_catatan`);
+
+--
+-- Indexes for table `hutang`
+--
+ALTER TABLE `hutang`
+  ADD PRIMARY KEY (`id_hutang`);
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indexes for table `pemasukan`
+--
+ALTER TABLE `pemasukan`
+  ADD PRIMARY KEY (`id_pemasukan`),
+  ADD KEY `id_sumber` (`id_sumber`);
+
+--
+-- Indexes for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD PRIMARY KEY (`id_pengeluaran`),
+  ADD KEY `id_sumber` (`id_sumber`);
+
+--
+-- Indexes for table `sumber`
+--
+ALTER TABLE `sumber`
+  ADD PRIMARY KEY (`id_sumber`);
+
+--
+-- Indexes for table `uang`
+--
+ALTER TABLE `uang`
+  ADD PRIMARY KEY (`id_uang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `catatan`
+--
+ALTER TABLE `catatan`
+  MODIFY `id_catatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `hutang`
+--
+ALTER TABLE `hutang`
+  MODIFY `id_hutang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pemasukan`
+--
+ALTER TABLE `pemasukan`
+  MODIFY `id_pemasukan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `sumber`
+--
+ALTER TABLE `sumber`
+  MODIFY `id_sumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `uang`
+--
+ALTER TABLE `uang`
+  MODIFY `id_uang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
